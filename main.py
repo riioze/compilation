3a56bbeefb4def559685f8d2e89fe7b4f3c48931
Nino Keravel <61903425+riioze@users.noreply.github.com>
2025-08-28 17:34:28 +0200

    Jules added documentation

--- main.py
+++ main.py
@@ -32,23 +32,22 @@ class Node:
 
 
 class Lexer:
+    def __init__(self, text:str):
+        """ 
+        Méthode d'initialisation de la classe
 
-
-    def __init__(self, text : str):
-        self.pointer_pos = 0
-        self.current_line = 0
-        self.current_col = 0
-
-        self.text = text
+        @params
+        Entrée : text, type String, code à compiler
+        Sortie : None
+        """
+        self.pointer_pos : int = 0
+        self.current_line : int = 0
+        self.current_col : int = 0
 
         self.current_token = Token("tok_start",(-1,-1))
         self.last_token = Token("tok_start",(-1,-1))
 
-        self.spacing_chars = [
-            ' ',
-            '\n',
-            '\t',
-        ]
+        self.spacing_chars = [' ', '\t', '\n']
 
         self.keywords = [
             "int",
@@ -66,68 +65,165 @@ class Lexer:
             "rec"
         ]
 
+        self.text = text
+    
+    # -----
+    # Vérification de position du curseur
+    # -----
     def test_eof(self):
+        """ 
+        Renvoi de l'égalité entre la position du pointeur parcourant le code 
+        et la longueur du code
+
+        S'ils sont égaux, alors le code a fini d'être parcouru
+
+        @params
+        Entrée : None
+        Sortie : None
+        """
         return self.pointer_pos == len(self.text)
             
     def eof_tok(self):
+        """ 
+        Création du token 'End Of File', signifiant que le curseur est après le 
+        dernier caratère du code, il a donc fini d'être parcouru
+
+        @params
+        Entrée : None
+        Sortie : None
+        """
         return Token("tok_eof",(self.current_line,self.current_col))
     
-
-    def is_number(self,character:str):
-        return ord('0') <= ord(character) and ord(character) <= ord('9')
+    # -----
+    # Vérification du type de caratère rencontré
+    # -----
+    def is_number(self, character:str):
+        """
+        Renvoie si le caractère étudié est un chiffre ou non en utilisant la table ASCII
+        
+        @params
+        Entrée : character, type string, caractère du code rencontré
+        Sortie : Boolean, résultat de la comparaison
+        """
+        return ord('0' ) <= ord(character) <= ord('9')
     
     def is_letter(self,character:str):
-        return (ord('A') <= ord(character) and ord(character) <= ord('Z')) or (ord('a') <= ord(character) and ord(character) <= ord('z'))
+        """
+        Renvoie si le caractère étudié est une lettre ou non en utilisant la table ASCII
+        
+        @params
+        Entrée : character, type string, caractère du code rencontré
+        Sortie : Boolean, résultat de la comparaison
+        """
+        return (ord('A') <= ord(character) <= ord('Z')) or (ord('a') <= ord(character) <= ord('z'))
     
     def is_alpha_num(self,character:str):
+        """
+        Renvoie si le caractère étudié est alphanumérique la méthode is_number et is_letter
+        
+        @params
+        Entrée : character, type string, caractère du code rencontré
+        Sortie : Boolean, résultat de la comparaison
+        """
         return self.is_number(character) or self.is_letter(character)
+    
+    # -----
+    # Parcours du code dans le but de trouver le token suivant
+    # -----
 
     def next_token(self):
+        """
+        La fonction next_token recherche dans le code à compiler le prochain token en faisant
+        avancer le pointeur le parcourant.
+
+        La fonction commence par mettre à jour les token, passe les différents espace puis
+        convertit les caratères rencontrés en token.
+
+        @params
+        Entrée : None
+        Sortie : None
+        """
         
+        # Actualisation des tokens
         self.last_token = self.current_token
 
+        # Vérification de la position du curseur par rapport à la fin du code
         if self.test_eof():
+            # Si le code a été entièrement parcouru, le token passe en end of file
             self.current_token = self.eof_tok()
+            # Arret de la méthode
             return
 
+        # ---
+        # Gestion des espaces et retour à la ligne
+        # ---
+
         while (self.text[self.pointer_pos] in self.spacing_chars): # skipping spaces
+            
+            # Augmentation de l'indice à regarder
             self.pointer_pos += 1
 
+            # S'il a dépassé la fin du code, token end of file et arret
             if self.test_eof():
                 self.current_token = self.eof_tok()
+
+                # Le parcourt ne renvoie rien car pas de token utilisable après les espaces
                 return
 
+            # Mise à jour de la position du curseur
             self.current_col += 1
             if self.text[self.pointer_pos] == '\n':
                 self.current_col = 0
                 self.current_line+=1
+        
+        # ---
+        # Le pointeur rencontre des caractères, attribution de token
+        # ---
 
-
-
+        # Rencontre de chiffre/nombre
         if self.is_number(self.text[self.pointer_pos]): # parse full number
+
+            # Création du nombre
             current_number = ''
-            
+
+            # Ajout de chaque chiffre du nombre caractère par caractère          
             while not self.test_eof() and self.is_number(self.text[self.pointer_pos]):
                 current_number += self.text[self.pointer_pos]
                 self.pointer_pos+=1
                 
-            
+            # Le nombre est complet, création de token
             self.current_token = Token("tok_const",(self.current_line,self.current_col),token_value=int(current_number))
+            
+            # Le token a été trouvé, arret de la méthode
             return
 
+        # Rencontre de lettre/mot
         if self.is_letter(self.text[self.pointer_pos]):
+
+            # Création du mot
             current_word = ''
 
+            # Tant que le pointeur n'est pas à la fin du code et qu'il rencontre des caratères
             while not self.test_eof() and self.is_alpha_num(self.text[self.pointer_pos]):
+                # Ajout de la lettre ou du chiffre au mot
                 current_word += self.text[self.pointer_pos]
+
                 self.pointer_pos+=1
 
+            # Vérification de la présence du mot dans les mots clefs réservés
             if current_word in self.keywords:
+                # Création du token particulier réservé au mot-clef
                 self.current_token = Token("tok_"+current_word,(self.current_line,self.current_col),token_string=current_word)
             else:
+                # Création d'un token d'identification
                 self.current_token = Token("tok_ident",(self.current_line,self.current_col),token_string=current_word)
+            
+            # Token trouvé, arret de la méthode
             return
         
+        # ---
+        # Si le caractère rencontré est un token en lui-même, renvoi du token correspondant
+        # ---
         match self.text[self.pointer_pos]:
             case '+':
                 self.current_token = Token("tok_+",(self.current_line,self.current_col))
@@ -145,36 +241,42 @@ class Lexer:
                 self.current_token = Token("tok_%",(self.current_line,self.current_col))
             
             case '&':
+                # Distinction entre '&' et '&&'
                 if self.pointer_pos + 1 < len(self.text) and self.text[self.pointer_pos+1] == '&':
                     self.current_token = Token("tok_&&",(self.current_line,self.current_col))
                 else:
                     self.current_token = Token("tok_&",(self.current_line,self.current_col))
             
             case '|':
+                # Le token '|' n'existe pas, vérification de la présence de '||'
                 if self.pointer_pos + 1 < len(self.text) and self.text[self.pointer_pos+1] == '|':
                     self.current_token = Token("tok_||",(self.current_line,self.current_col))
                 else:
                     raise ValueError(f"Token | unkown at pos (line = {self.current_line} col = {self.current_col}) did you mean \"||\" ?")
             
             case '!':
+                # Distinction entre '!' et '!='
                 if self.pointer_pos + 1 < len(self.text) and self.text[self.pointer_pos+1] == '=':
                     self.current_token = Token("tok_!=",(self.current_line,self.current_col))
                 else:
                     self.current_token = Token("tok_!",(self.current_line,self.current_col))
             
             case '=':
+                # Distinction entre '=' et '=='
                 if self.pointer_pos + 1 < len(self.text) and self.text[self.pointer_pos+1] == '=':
                     self.current_token = Token("tok_==",(self.current_line,self.current_col))
                 else:
                     self.current_token = Token("tok_=",(self.current_line,self.current_col))
 
             case '<':
+                # Distinction entre '<' et '<='
                 if self.pointer_pos + 1 < len(self.text) and self.text[self.pointer_pos+1] == '=':
                     self.current_token = Token("tok_<=",(self.current_line,self.current_col))
                 else:
                     self.current_token = Token("tok_<",(self.current_line,self.current_col))
 
             case '>':
+                # Distinction entre '>' et '>='
                 if self.pointer_pos + 1 < len(self.text) and self.text[self.pointer_pos+1] == '=':
                     self.current_token = Token("tok_>=",(self.current_line,self.current_col))
                 else:
@@ -206,16 +308,32 @@ class Lexer:
 
             case other:
                 raise ValueError(f"Token {other} unkown at pos (line = {self.current_line} col = {self.current_col})")
-
-        self.pointer_pos += 1
-
-
+        self.pointer_pos+=1
+    
     def accept(self,token_type:str):
+        """
+        Vérification du type du current_token par rapport au type fourni en argument.
+        Si le type du current_token n'est pas le bon, un message d'erreur est envoyé
+
+        @params
+        Entrée : token_type, type string, type du token 
+        Sortie : None
+        """
         if not self.check(token_type):
             line, col = self.current_token.token_pos
             raise ValueError(f"wrong token at pos ({line = } {col = }) expected a token of type {token_type}")
 
     def check(self,token_type:str):
+        """
+        Vérification du type du current_token par rapport au type fourni en argument.
+        Si le type est le bon, appel à next_token() pour trouver la token d'avant,
+        renvoi de 'True'
+        Si le type du current_token n'est pas le bon, 'False' est renvoyé
+
+        @params
+        Entrée : token_type, type string, type du token 
+        Sortie : Boolean, résultat de la comparaison
+        """
         if self.current_token.token_type == token_type:
             self.next_token()
             return True
