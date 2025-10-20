17181c0a0d2676bb50091f80a721761224060b58
Nino Keravel <61903425+riioze@users.noreply.github.com>
2025-10-20 16:40:00 +0200

    implement print et println

--- code.c
+++ code.c
@@ -4,7 +4,6 @@ int main() {
 
     int *p;
 
-    debug *0;
 
     *p = malloc(5);
 
@@ -13,13 +12,7 @@ int main() {
         *(p+x) = 3;
     }
 
-    debug *p;
-
-    i = recv;
-
-    send i+1;
-
-    send 10;
+    print(*p);
 
     return 0;
 }
--- std.c
+++ std.c
@@ -5,3 +5,19 @@ int *malloc(int n){
     return p;
 }
 
+int print(int n){
+    int d; d = n%10;
+    int r; r = n/10;
+    int nl;
+    nl=0;
+    if (r!0) 
+        nl = print(r) + 1;
+    send 48+d;
+    return nl;
+}
+
+int println(int n) {
+    int nl = print(n);
+    send 10;
+    return nl; 
+}
